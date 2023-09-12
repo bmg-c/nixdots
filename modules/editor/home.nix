a7d8a818d2c9e854a36ad9267836e9c704f3b314
bmg-c <letalbark@gmail.com>
2023-09-12 20:51:30 +0700

    ueoa

--- modules/editor/home.nix
+++ modules/editor/home.nix
@@ -1,5 +1,34 @@
-{pkgs, ...}: let
-  jdtls = import ../../pkgs/jdtls.nix;
+{
+  pkgs,
+  lib,
+  ...
+}: let
+  jdtls = pkgs.stdenv.mkDerivation {
+    pname = "jdt-language-server";
+    version = "1.26.0";
+
+    src = pkgs.fetchurl {
+      url = https://www.eclipse.org/downloads/download.php?file=/jdtls/milestones/1.26.0/jdt-language-server-1.26.0-202307271613.tar.gz;
+      sha256 = "sha256-ul/l7jsqg5UofiSu8gzm4Xg0z46HcRfmyqysamiKbFM=";
+    };
+
+    nativeBuildInputs = with pkgs; [makeWrapper];
+
+    buildPhase = ''
+      mkdir -p jdt-language-server
+      tar xfz $src -C jdt-language-server
+    '';
+
+    installPhase = ''
+      mkdir -p $out/bin $out/libexec
+      cp -a jdt-language-server $out/libexec
+      makeWrapper $out/libexec/jdt-language-server/bin/jdtls $out/bin/jdtls --prefix PATH : ${lib.makeBinPath [pkgs.python3]}
+    '';
+
+    dontUnpack = true;
+    dontPatch = true;
+    dontConfigure = true;
+  };
 in {
   programs.helix = {
     enable = true;
